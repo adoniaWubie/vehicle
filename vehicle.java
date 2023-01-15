693eb3bfbc616f6fbc4fead73a6c8074f1d29271
adoniaWubie <117729951+adoniaWubie@users.noreply.github.com>
2023-01-15 12:34:23 +0300

    Create vehicle.java

---
+++ vehicle.java
@@ -0,0 +1,49 @@
+abstract class Vehicle{
+    abstract void start () ;
+    abstract void stop ();
+    abstract void accelerate ();
+    abstract void brake ();
+}
+     
+class Car extends Vehicle{
+    void start () { 
+    // code here
+    }
+    void stop () { 
+    // code here
+    }
+    void accelerate () { 
+    // code here
+    }
+    void brake () { 
+    // code here
+    }
+}
+class Bike extends Vehicle{
+    void start () { 
+    // code here
+    }
+    void stop () { 
+    // code here
+    }
+    void accelerate () { 
+    // code here
+    }
+    void brake () { 
+    // code here
+    }
+}
+class Scooter extends Vehicle{
+    void start () { 
+    // code here
+    }
+    void stop () { 
+    // code here
+    }
+    void accelerate () { 
+    // code here
+    }
+    void brake () { 
+    // code here
+    }
+}
